e0a77c82aae252461268b62d8f53d91330407d00
wiggy99 <57639380+wiggy99@users.noreply.github.com>
2021-06-18 11:58:42 +1200

    Added port functionality and documentation

--- servius.sh
+++ servius.sh
@@ -4,13 +4,14 @@ while test $# -gt 0; do
     -h|--help)
       echo "$package - attempt to serve file"
       echo " "
-      echo "$package [option] target-file"
+      echo "$package [option] target-file [-p (optional)] [port (optional)]"
       echo " "
       echo "options:"
       echo "-h, show help page"
       echo "-l, locally serve file"
       echo "-i, serve file over internet"
       echo "-d, host dedicated server"
+      echo "-p, specify a port otherwise default to 8000"
       exit 0
       ;;
     -l)
@@ -18,7 +19,13 @@ while test $# -gt 0; do
       zip -r $1.zip $1
       mkdir serve
       mv $1.zip serve
-      python3 -m http.server 8000 --directory serve/
+      if [$1 = "-p"]
+      shift
+      then
+        python3 -m http.server $1 --directory serve/
+      else
+        python3 -m http.server 8000 --directory serve/
+      fi
       shift
       ;;
     -i)
